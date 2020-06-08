3e1d1770bd9ffd980edfe80cc4540582ca91b739
doylemark <39464373+doylemark@users.noreply.github.com>
2020-06-08 19:59:08 +0100

    Remove extraneous variables

--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 	"github.com/doylemark/flightsim-bot/commands/env"
 )
 
-// ScreenshotChannelID The channel the bot will send messages to
-var ScreenshotChannelID string
-
-// GuildID The guild id of the server the screenshot competitions will be taking place in
-var GuildID string
-
 func main() {
 	env.LoadEnvVars()
 	database.Connect()
